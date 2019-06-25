2dcb92879642524fc6d04f3be27d194c216cc7cc
huangwei <1845739048@qq.com>
2019-06-25 11:14:00 +0800

    2019-6-25-11:13-com

---
+++ Q1_50/022.cpp
@@ -0,0 +1,66 @@
+// 22括号生成
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<char> stack;
+        vector<string> ret;
+        process(n ,'(', 2*n, stack, 0, ret);
+        return ret;
+    }
+
+    void process(int n, char c, int depth, vector<char> &stack, int moreLeft, vector<string> &ret) {
+        if (c == '(') {
+            if (depth == 1)
+                return;
+            if (moreLeft >= n)
+                return;
+            stack.push_back(c);
+            moreLeft++;
+            process(n, '(', depth-1, stack, moreLeft, ret);
+            process(n, ')', depth-1, stack, moreLeft, ret);
+        } else {
+            if (moreLeft == 0)
+                return;
+            stack.push_back(c);
+            if (depth == 1) {
+                if (moreLeft == 1)
+                    ret.push_back(genStringFromStack(stack));
+            } else if (moreLeft > 0) {
+                moreLeft--;
+                process(n, '(', depth-1, stack, moreLeft, ret);
+                process(n, ')', depth-1, stack, moreLeft, ret);
+            }
+        }
+        stack.pop_back();
+        return;
+    }
+
+    string genStringFromStack(vector<char> &stack) {
+        string ret;
+        for (int i = 0; i < stack.size(); i++) {
+            ret.push_back(stack[i]);
+        }
+        return ret;
+    }
+
+    void printStack(vector<char> &stack) {
+        for (int i = 0; i < stack.size(); i++) {
+            cout << stack[i];
+        }
+        cout <<endl;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<string> ret;
+    ret = s.generateParenthesis(4);
+    for (string str : ret) {
+        cout << str << endl;
+    }
+}
---
+++ Q1_50/046.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ret;
+        vector<int> cur;
+        backTrace(ret, cur, nums);
+
+        return ret;
+    }
+
+    void backTrace(vector<vector<int>> &ret, vector<int> &cur, vector<int>& nums) {
+        if (cur.size() == nums.size()) {
+            ret.push_back(cur);
+            cur.pop_back();
+            return;
+        }
+        for (unsigned int i = 0; i < nums.size(); i++) {
+            if (!contain(cur, nums[i])) {
+                cur.push_back(nums[i]);
+                backTrace(ret, cur, nums);
+            }
+        }
+        cur.pop_back();
+    }
+
+    bool contain(vector<int> &vec, int v) {
+        for (int i = 0; i < vec.size(); ++i) {
+            if (vec[i] == v)
+                return true;
+        }
+        return false;
+    }
+
+    void printVector(vector<int>  &vec) {
+        for (unsigned int i = 0; i < vec.size(); ++i) {
+            cout << vec[i] << " ";
+        }
+        cout <<endl;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> ret;
+    vector<int> nums {
+        1, 2, 3
+    };
+    ret = s.permute(nums);
+    for (int i = 0; i < ret.size(); ++i) {
+        s.printVector(ret[i]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
